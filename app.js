2cc4f436846a11f78db8ab990a266996a6ae8257
mikeangelo1212 <miguelmounstro@hotmail.com>
2024-11-27 01:08:47 -0600

    finalizacion

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//Esto te devuelve al principio
+//window.scrollTo(0,0)
+
+
+//metodo que observa si esta a la vista las clases hidden
+//que en si decide si se muestra o no lo que traiga pegado este atributo
 const observer= new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         console.log(entry)
